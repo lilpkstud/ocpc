632eecf05fe6dd791d7e1bf757d09714fa51502f
lilpkstud <lilpkstud@hotmail.com>
2019-08-07 13:50:12 -0700

    Google Calendar API
    Allowing users to see their calendar events in Bootstrap card

---
+++ views/calendar.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>OCPC Google Calendar</title>
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+</head>
+<body>
+  <div class="container">
+   
+   <div class="row">
+        <div id="eventTemplate" style="display: none">
+            <div class="col-3 event card" style="width: 18rem; margin: 20px;">
+                <img src="/img/logo.png" class="card-img-top" alt="Event #1">
+                <div class="card-body">
+                    <h3 class="evTitle card-title">Friday Service (Event #1)</h3>
+                    <div class="evDate">June 13th 2019 at 7:30 to 7:50pm</div>
+                    <div class="evLocation">Location of the Place</div>
+                    <div class="evDescription">Description of the events</div>
+                </div>
+            </div>
+        </div>
+   </div>
+
+    <div id="events" class="row">
+    </div>
+  
+    <!--Add buttons to initiate auth sequence and sign out-->
+    <button id="authorize_button" style="display: none;">Authorize</button>
+    <button id="signout_button" style="display: none;">Sign Out</button>
+
+    <pre id="content" style="white-space: pre-wrap;"></pre>
+  </div>
+    <!-- JQuery -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <!-- AJAX -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <!-- Google Calendar JS -->
+    <script src="/js/calendar.js"></script>
+    <!-- Google API -->
+    <script async defer src="https://apis.google.com/js/api.js"
+        onload="this.onload=function(){};handleClientLoad()"
+        onreadystatechange="if (this.readyState === 'complete') this.onload()">
+    </script>
+    <!-- Moment -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.24.0/moment.min.js" integrity="sha256-4iQZ6BVL4qNKlQ27TExEhBN1HFPvAvAMbFavKKosSWQ=" crossorigin="anonymous"></script>
+</body>
+</html>
